0de7061a3377641abd8a3c3e7cc1b062d4abbf75
Alejandro Noguera <janoguerab@unal.edu.co>
2020-09-19 14:27:55 -0500

    Added Register route - function

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const apiPlatosRouter = require("./api/platos");
+const apiUsuariosRouter = require("./api/usuarios");
 
 router.use("/platos", apiPlatosRouter);
+router.use("/usuarios", apiUsuariosRouter);
 
 module.exports = router;
---
+++ routes/api/usuarios.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs"); // Uso de bcrypt para cifrar la contraseña
+const { Usuario } = require("../../db");
+const { check, validationResult } = require("express-validator"); //Agregar validaciones sobre los datos enviados en el body de la petición
+
+router.post(
+  "/register",
+  [
+    check("usuario", "El nombre de usario es obligatorio").not().isEmpty(),
+    check("password", "El password es obligatorio").not().isEmpty(),
+    check("email", "El email no es correcto").isEmail(),
+    check("direccion", "La dirección es obligatoria").not().isEmpty(),
+    check("rol", "El Rol es obligatorio").not().isEmpty(),
+  ],
+  async (req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(422).json({ errores: errors.array() });
+    }
+    req.body.password = bcrypt.hashSync(req.body.password, 10); // Se cifra la contraseña 10 veces
+    const usuario = await Usuario.create(req.body);
+    res.json(usuario);
+  }
+);
+
+module.exports = router;
