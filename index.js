da4957365aa71d1c449160bfa6a1bcbc16422fc0
Alejandro Noguera <janoguerab@unal.edu.co>
2020-09-19 19:50:31 -0500

    variable de entorno para el puerto de la api

--- index.js
+++ index.js
@@ -5,14 +5,21 @@ const apiRouter = require("./routes/api");
 
 const app = express();
 
+// Levanta la base de datos y crea las tablas si estas no existen
 require("./db");
+
+//Trae las variables almacenadas en el Environment
+require("./env");
 
 app.use(cors());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
+// Agrega las rutas localhost:PORT/api/RUTAS
 app.use("/api", apiRouter);
 
-app.listen(3000, () => {
-  console.log("Sevidor eschucando en http://localhost:3000");
+// Iniciar el servidor por el puerto definido en las variables del sistema por defecto 3000
+const port = process.env.APPI_PORT ? process.env.APPI_PORT : 3000;
+app.listen(port, () => {
+  console.log(`Sevidor eschucando en http://localhost:${port}`);
 });
